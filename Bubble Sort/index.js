98db265f78529390a8e7e05c171534b7111172d8
Vishnu <dummy.vishnu66@gmail.com>
2024-11-18 09:58:45 +0530

    added bubble sort

---
+++ Bubble Sort/index.js
@@ -0,0 +1,18 @@
+/**
+ *  ********** Bubble Sort **********
+ *  1. Push the max element to the end of the array 
+ *  by adjust swaps
+ */
+
+
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+
+function bubblesort (arr) {
+    let n = arr.length;
+    for(let i = 0; i)
+}
